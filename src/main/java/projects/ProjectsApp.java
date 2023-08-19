a7636536818d3f23b08e2a66e6b73641b04bc65d
prakashkhadka7524 <prakashkhadka7524@outlook.com>
2023-08-19 12:30:11 -0700

    first commit

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,13 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		DbConnection dbConnection= new DbConnection();
+		dbConnection.getConnection();
+	}
+
+}
